42cd67323ee0e152d82a849b0310609b9bb88e57
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-08-29 16:40:32 -0700

    Add admin interface; add form for adding comments

--- tumblelog/views.py
+++ tumblelog/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
+from flask.ext.mongoengine.wtf import model_form
+
 from tumblelog.models import Post, Comment
 
 
@@ -13,9 +15,43 @@ class ListView(MethodView):
 
 
 class DetailView(MethodView):
+
+    form = model_form(Comment, exclude=['created_date'])
+
+    def get_context(self, slug):
+        """ Get the default context for GET and POST requests. """
+        post = Post.objects.get_or_404(slug=slug)
+        form = self.form(request.form)
+
+        context = {
+            'post': post,
+            'form': form
+        }
+
+        return context
+
     def get(self, slug):
         post = Post.objects.get_or_404(slug=slug)
         return render_template('posts/detail.html', post=post)
+
+    def post(self, slug):
+        context = self.get_context(slug)
+        form = context.get('form')
+
+        # If comment is valid, append comment to post
+        if form.validate():
+            comment = Comment()
+            form.populate_obj(comment)
+
+            post = context.get('post')
+            post.comments.append(comment)
+            post.save()
+
+            return redirect(url_for('posts.detail', slug=slug))
+
+        return render_template('posts/detail.html', **context)
+
+
 
 # Register the urls
 posts.add_url_rule('/', view_func=ListView.as_view('list'))
